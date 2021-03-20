5e5cbb3cc9a8d5421bc02a3c63cf7f65e75cf7ce
Cam Saul <github@camsaul.com>
2021-03-19 21:11:06 -0700

    Fix ns checking with reader conditionals

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject lein-check-namespace-decls "1.0.2"
+(defproject lein-check-namespace-decls "1.0.3"
   :min-lein-version "2.5.0"
 
   :dependencies
-  [[org.clojure/tools.namespace "0.2.11"]
-   [cider/cider-nrepl "0.21.1"]
-   [mvxcvi/puget "1.1.0"]
-   [refactor-nrepl "2.4.0"]]
+  [[org.clojure/tools.namespace "1.1.0"]
+   [cider/cider-nrepl "0.25.9"]
+   [mvxcvi/puget "1.3.1"]
+   [refactor-nrepl "2.5.1"]]
 
   :description
   "Linter that checks that namespace decls for files in a Leinigen project's `:source-paths` are cleaned the way
@@ -13,6 +13,11 @@
 
   :license
   {:name "EPL-2.0"
-   :url "https://www.eclipse.org/legal/epl-2.0/"}
+   :url  "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :profiles
+  {:dev
+   {:dependencies      [[org.clojure/clojure "1.10.3"]]
+    :eval-in-leiningen false}}
 
   :eval-in-leiningen true)
