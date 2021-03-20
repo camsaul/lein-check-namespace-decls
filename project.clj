9a5595120db8e6bca6b8f7bf202058dd34196c81
Cam Saul <github@camsaul.com>
2021-03-19 21:33:36 -0700

    Change LICENSE from EPL v1.0 to EPL v2.0; add URL to project.clj

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
 (defproject lein-check-namespace-decls "1.0.3"
   :min-lein-version "2.5.0"
 
+  :description
+  "Linter that checks that namespace decls for files in a Leinigen project's `:source-paths` are cleaned the way
+  `clj-refactor` would clean them."
+  :url "https://github.com/camsaul/lein-check-namespace-decls"
+  :license
+  {:name "EPL-2.0"
+   :url  "https://www.eclipse.org/legal/epl-2.0/"}
+
   :dependencies
   [[org.clojure/tools.namespace "1.1.0"]
    [cider/cider-nrepl "0.25.9"]
    [mvxcvi/puget "1.3.1"]
    [refactor-nrepl "2.5.1"]]
-
-  :description
-  "Linter that checks that namespace decls for files in a Leinigen project's `:source-paths` are cleaned the way
-  `clj-refactor` would clean them."
-
-  :license
-  {:name "EPL-2.0"
-   :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
   :profiles
   {:dev
