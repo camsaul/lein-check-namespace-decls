e2c92156088a1739acab79341e68370c7fff30f0
Cam Saul <github@camsaul.com>
2021-06-17 15:02:16 -0700

    Add :test alias, I'm hoping this tricks CircleCI into using the correct Clojure version

--- project.clj
+++ project.clj
@@ -9,19 +9,24 @@
   {:name "EPL-2.0"
    :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
+  :aliases
+  {"test" ["with-profile" "+test" "test"]}
+
   :dependencies
   [[org.clojure/tools.namespace "1.1.0"]
-   [cider/cider-nrepl "0.25.9"]
+   [cider/cider-nrepl "0.26.0"]
    [mvxcvi/puget "1.3.1"]
    [refactor-nrepl "2.5.1"]]
 
   :profiles
   {:dev
    {:dependencies      [[org.clojure/clojure "1.10.3"]
-                        [pjstadig/humane-test-output "0.10.0"]]
+                        [pjstadig/humane-test-output "0.11.0"]]
     :injections        [(require 'pjstadig.humane-test-output)
                         (pjstadig.humane-test-output/activate!)]
-    :eval-in-leiningen false}}
+    :eval-in-leiningen false}
+
+   :test {}}
 
   :eval-in-leiningen true
 
